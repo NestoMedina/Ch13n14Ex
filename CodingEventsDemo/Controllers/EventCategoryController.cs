4fc02bf0d5f273d45bfbe8eccc2641fdce130c5e
NestoMedina <emedina670@hotmail.com>
2021-08-04 17:34:03 -0400

    finished Ch19 studio

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,36 @@ namespace CodingEventsDemo.Controllers
         {
             List<EventCategory> events = _context.Category.ToList();
             return View(events);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addModel = new AddEventCategoryViewModel();
+            return View(addModel);
+        }
+
+        [HttpPost("/EventCategory/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel newModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newInput = new EventCategory
+                {
+                    Name = newModel.Name
+                };
+                _context.Category.Add(newInput);
+                _context.SaveChanges();
+                return Redirect("/EventCategory/Index");
+            }
+            return View("/EventCategory/Create", newModel);
+        }
+
+        public IActionResult EventInfo()
+        {
+            List<EventCategory> events = _context.Category.ToList();
+            ViewBag.events = events;
+            return View();
         }
     }
 }
---
+++ CodingEventsDemo/ViewModel/AddEventCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModel
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage="Category name is required.")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
